0ff9c4bb9451a3d2b53e2a50d559d3f0229cfe33
MikaelCarpenter <mikaelcarp.dev@gmail.com>
2014-10-04 21:52:30 -0700

    personalized my content and added scroll feature for the nav links

--- public/main.js
+++ public/main.js
@@ -1,3 +1,18 @@
 $(document).on('ready', function() {
-  
+	smoothScroll(600);
 });
+
+// smoothScroll is just being defined here, but is being called in the on ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+		var target = $( $(this).attr('href') );
+
+		if( target.length ) {
+			event.preventDefault();
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, duration);
+		}
+	});
+}
