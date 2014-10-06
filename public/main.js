43f1e77e17d83c9e67fa59ac48ae890227c928b0
MikaelCarpenter <mikaelcarp.dev@gmail.com>
2014-10-05 23:42:06 -0700

    done for the night. ajax work content tommorrow.

--- public/main.js
+++ public/main.js
@@ -22,13 +22,15 @@ function workBelt() {
 
 	$('.thumb-unit').click(function() {
 
-		$('.work-belt').css('left', '-100%')
+		$('.work-belt').css('left', '-100%');
+		$('.work-container').show();
 
 	});
 
 	$('.work-return').click(function() {
 
-		$('.work-belt').css('left', '0%')
+		$('.work-belt').css('left', '0%');
+		$('.work-container').hide(800);
 
 	})
 
